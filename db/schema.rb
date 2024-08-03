1484ff770cb83363a0d31692e4a08887cc6ef8da
YOUR NAME <YOUR@EMAIL.com>
2024-08-03 12:16:39 -0600

    Create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_25_171433) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_03_181513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_25_171433) do
     t.string "email"
     t.text "message"
     t.string "source_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
