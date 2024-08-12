bab5cb50d25efb02a2239598fbe0eb296155bc9b
YOUR NAME <YOUR@EMAIL.com>
2024-08-12 14:23:05 -0600

    revert to old commit

---
+++ routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'user_token/create'
+  namespace :api do
+    namespace :v1 do
+      post 'gravity_forms/fetch_and_save/:company_name', to: 'gravity_forms#fetch_and_save'
+      get 'gravity_forms/entries', to: 'gravity_forms#entries'
+      get 'call_rail_data/fetch_and_store', to: 'call_rail_data#fetch_and_store'
+      get 'call_rail_data', to: 'call_rail_data#index'
+      get 'gravity_forms_data', to: 'gravity_forms#index'
+      get '/ws', to: 'your_controller#your_action'
+      post 'gravity_forms/fetch_and_save/:company_name', to: 'gravity_forms#fetch_and_save'
+      get 'gravity_forms/entries', to: 'gravity_forms#entries'
+    end
+  end
+end
