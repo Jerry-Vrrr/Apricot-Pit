1d77c1469586b74a29bd7e59fc23c1b10d5e8db6
YOUR NAME <YOUR@EMAIL.com>
2024-08-12 11:49:27 -0600

    update render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Exit on any command with non-zero status
 set -o errexit
 
+# Install the dependencies
 bundle install
+
+# Run any pending migrations
+bundle exec rake db:migrate
+
+# Precompile assets (only necessary if you're using Rails with assets)
+# Uncomment the line below if you have assets in your application
+# bundle exec rake assets:precompile
